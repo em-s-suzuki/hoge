7b38aef08f807b00cbb6b05a3011640b2d525b8a
Shinichiro Suzuki <s_suzuki@entermotion.jp>
2014-08-19 14:51:57 +0900

    move application folder

---
+++ application/controllers/syscheck.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Syscheck extends CI_Controller
+{
+    /**
+     * Syscheck index
+     *
+     * @params none
+     * @return 
+     */
+    public function index()
+    {
+        try {
+            $this->load->library('SyscheckClass');
+            // @@ see オブジェクト名は小文字
+            $now = $this->syscheckclass->doCheck()->now;
+            
+            $render = 
+                array(
+                    'now' => $now,
+                );
+            
+            $this->load->view('syscheck', $render);
+            
+        } catch (Exception $e) {
+            print $e->getMessage();
+        }
+    }
+}
